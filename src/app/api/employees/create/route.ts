20d2289be085610d860098041c57ae2223fd8385
aamir <aamirkhan@engineer.com>
2024-07-06 18:28:16 +0500

    feat: added a route to add new employees

--- src/app/api/employees/create/route.ts
+++ src/app/api/employees/create/route.ts
@@ -1,5 +1,25 @@
-import { NextApiResponse, NextApiRequest } from "next";
+import { dbQuery } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextApiRequest) {
-	return Response.json({ test: "passed" });
+interface Employee {
+	name: string;
+	department: string;
+	title: string;
+	email: string;
+}
+export async function POST(req: NextRequest) {
+	try {
+		const { email, title, name, department } = await req.json();
+
+		const res = await dbQuery(
+			"INSERT INTO `Employee`(`email`, `name`, `title`, `department`) VALUES (?, ?, ?, ?)",
+			[email, name, title, department]
+		);
+		return NextResponse.json(
+			{ success: true, massage: "record inserted successfully" },
+			{ status: 200 }
+		);
+	} catch (error) {
+		console.log("Error while inserting the record: ", error);
+	}
 }
